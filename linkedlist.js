405abfcce1d9c70ec33ae8c2e57e78bc86fb6cdf
Brett Beers <beersbr@gmail.com>
2013-03-31 21:18:54 -0600

    made it a module

--- linkedlist.js
+++ linkedlist.js
@@ -1,159 +1,145 @@
-/** @class ListNode Linked List Node */
-function ListNode(value)
-{
-    this.value = value;
-    this.next = null;
-    this.prev = null;
-}
+var List = (function(list){
 
-/** @class List Linked List */
-function List()
-{
-    this._length = 0;
-    this.head = null;
-    this.tail = null;
-
-    Object.defineProperty(this, "length", {
-      get: function(){
-          return this._length;
-      }
-    });
-
-    this.push = function(value)
+    list.ListNode = function(value)
     {
-        if(this.head == null)
-        {
-            this.head = new ListNode(value);
-            this.tail = this.head;
-        }
-        else
-        {
-            var curNode = this.tail;
-            curNode.next = new ListNode(value);
-            curNode.next.prev = curNode;
-            this.tail = curNode.next;
-        }
-
-        this._length += 1;
-
-        return true;
+        this.value = value;
+        this.next = null;
+        this.prev = null;
     }
 
-    this.removeValue = function(value)
+    list.List = function()
     {
-        if(this.head == null)
-            return false;
+        this._length = 0;
+        this.head = null;
+        this.tail = null;
 
-        if(this.head.value == value)
-        {
-            var node = this.head;
-            this.head = this.head.next;
-            node = null;
-            this._length -= 1;
-            return true;
-        }
+        Object.defineProperty(this, "length", {
+          get: function(){
+              return this._length;
+          }
+        });
 
-        var curNode = this.head.next;
-        while(curNode != null)
+        this.push = function(value)
         {
-            if(curNode.value == value)
+            if(this.head == null)
             {
-                curNode.prev.next = curNode.next;
-
-                console.log(curNode.prev.next);
-
-                if(curNode == this.tail)
-                    this.tail = curNode.prev;
-                else
-                    curNode.next.prev = curNode.prev;
-
-                curNode = null;
-                this._length -= 1;
-                return true;
+                this.head = new ListNode(value);
+                this.tail = this.head;
             }
-            curNode = curNode.next;
-        }
-        return false;
-    }
+            else
+            {
+                var curNode = this.tail;
+                curNode.next = new ListNode(value);
+                curNode.next.prev = curNode;
+                this.tail = curNode.next;
+            }
 
-    /** @function {ListNode} removes the list node if found */
-    this.removeNode = function(node)
-    {
-        if(node == null)
-            return false;
+            this._length += 1;
 
-        if(node == this.head)
-        {
-            this.head = this.head.next;
-            this.head.prev = null;
-            node = null;
-            this._length -= 1;
             return true;
         }
 
-        var curNode = this.head.next;
-        while(curNode != null)
+        this.removeValue = function(value)
         {
-            if(node == curNode)
+            if(this.head == null)
+                return false;
+
+            if(this.head.value == value)
             {
-                node.prev.next = node.next;
+                var node = this.head;
+                this.head = this.head.next;
                 node = null;
                 this._length -= 1;
                 return true;
             }
-            curNode = curNode.next;
+
+            var curNode = this.head.next;
+            while(curNode != null)
+            {
+                if(curNode.value == value)
+                {
+                    curNode.prev.next = curNode.next;
+
+                    console.log(curNode.prev.next);
+
+                    if(curNode == this.tail)
+                        this.tail = curNode.prev;
+                    else
+                        curNode.next.prev = curNode.prev;
+
+                    curNode = null;
+                    this._length -= 1;
+                    return true;
+                }
+                curNode = curNode.next;
+            }
+            return false;
         }
 
-        return false;
-    }
-
-
-    this.eachNode = function(fn)
-    {
-        var curNode = this.head;
-        do
+        /** @function {ListNode} removes the list node if found */
+        this.removeNode = function(node)
         {
-            fn(n);
-            curNode = curNode.next;
-        }while(curNode != null);
+            if(node == null)
+                return false;
 
-    }
+            if(node == this.head)
+            {
+                this.head = this.head.next;
+                this.head.prev = null;
+                node = null;
+                this._length -= 1;
+                return true;
+            }
 
-    this.eachValue = function(fn)
-    {
-        var curNode = this.head;
-        while(curNode != null)
-        {
-            var node = curNode.next;
-            fn(curNode.value);
-            curNode = node;
+            var curNode = this.head.next;
+            while(curNode != null)
+            {
+                if(node == curNode)
+                {
+                    node.prev.next = node.next;
+                    node = null;
+                    this._length -= 1;
+                    return true;
+                }
+                curNode = curNode.next;
+            }
+
+            return false;
         }
 
-    }
 
-    this.log = function(n)
-    {
-        var curNode = this.head;
-
-        while(curNode != null)
+        this.eachNode = function(fn)
         {
-            console.log(curNode.value);
-            curNode = curNode.next;
+            var curNode = this.head;
+            do
+            {
+                fn(n);
+                curNode = curNode.next;
+            }while(curNode != null);
+
         }
-        return true;
-    }
-}
 
-function TestList()
-{
-    var l = new List();
+        this.eachValue = function(fn)
+        {
+            var curNode = this.head;
+            while(curNode != null)
+            {
+                var node = curNode.next;
+                fn(curNode.value);
+                curNode = node;
+            }
 
-    l.push(1);
-    l.push(2);
-    l.push(3);
-    l.push(5);
-    l.push(8);
-    l.push(13);
+        }
 
-    return l;
-}
+        this.log = function(n)
+        {
+            var curNode = this.head;
+
+            while(curNode != null)
+            {
+                console.log(curNode.value);
+                curNode = curNode.next;
+            }
+            return true;
+        }
+    })();
